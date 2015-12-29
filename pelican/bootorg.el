e200d603ed8a6ec3251c60905264ebcddd196e33
Brett Viren <brett.viren@gmail.com>
2015-12-28 17:29:56 -0700

    Add example of using Emacs org-mode as a ROOT 'notebook'

--- pelican/bootorg.el
+++ pelican/bootorg.el
@@ -7,3 +7,18 @@
 (require 'org)
 
 
+;; (setq org-latex-pdf-process
+;;       '("pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f" 
+;; 	"pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f" 
+;; 	"pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+;; (org-babel-do-load-languages
+;;  'org-babel-load-languages
+;;  '(
+;;    (sh . t)
+;;    (python . t)
+;;    (ditaa . t)
+;;    (dot . t)
+;;    (sqlite . t)
+;;    ))
+;; (setq org-confirm-babel-evaluate nil)
